22c7311b1333272a70032938e26ea1ed7d55b819
JoshuaLin78 <116842251+JoshuaLin78@users.noreply.github.com>
2023-01-16 21:48:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
+let timer;
+let challengeTimer;
+const ele = document.getElementById("timer");
+let timerON = false;
+let challengeMode = false;
+let sec = 0, minutes = 0;
+
+const wordLength = 5;
+const FLIP_ANIMATION_DURATION = 500;
+let correctWord;
+let wordVis = [false, false, false, false, false];
+let letterMap = new Map();
+
+const guessGrid = document.querySelector("[data-guess-grid]");
+const keyboard = document.querySelector("[data-keyboard]");
+
+const winPopup = document.getElementById("win-popup");
+const losePopup = document.getElementById("lose-popup");
+const challengePopup = document.getElementById("challenge-popup");
+const answerDisplay = document.getElementById("answer-display");
 
 startInteraction();
 getWordle();
 
-let timer;
-const ele = document.getElementById("timer");
-let timerON = false;
-let sec = 0, minutes = 0;
-
-const guessGrid = document.querySelector("[data-guess-grid]");
-const keyboard = document.querySelector("[data-keyboard]");
-const wordLength = 5;
-const FLIP_ANIMATION_DURATION = 500;
-let correctWord;
-
-const winPopup = document.getElementById("win-popup");
-const losePopup = document.getElementById("lose-popup");
+function letterCount(letter){
+    let count = 0;
+    for(let k = 0; k<correctWord.length; k++){
+       if(correctWord[k] === letter){
+            count++;
+       }
+    }
+    return count;
+}
 
 function getWordle(){                                           //Fetches a random 5 letter word using an API
     fetch('http://localhost:8000/word')
@@ -22,7 +38,7 @@ function getWordle(){                                           //Fetches a rand
         .then(json => {
             console.log(json);
             correctWord = json.toUpperCase();
-            document.getElementById('answer-display').InnerHTML = "The answer was: " + correctWord;
+            answerDisplay.textContent = "The answer was: " + correctWord;
         })
         .catch(err => console.log(err))
 }
@@ -45,9 +61,71 @@ function startKeyboard(){
     document.addEventListener("keydown", keyPress);
 }
 
+function openWinPopup(){
+    winPopup.classList.add("open-popup");
+    stopKeyboard();
+    keyboard.classList.add("disabled");
+    clearInterval(timer);
+    clearInterval(challengeTimer);
+    timerON = false;
+}
+
+function closeWinPopup(){
+    winPopup.classList.remove("open-popup");
+}
+
+function openLosePopup(){
+    losePopup.classList.add("open-popup");
+    stopKeyboard();
+    keyboard.classList.add("disabled");
+    clearInterval(timer);
+    clearInterval(challengeTimer);
+    timerON = false;
+}
+
+function closeLosePopup(){
+    losePopup.classList.remove("open-popup");
+}
+
+function openChallengePopup(){
+    challengePopup.classList.add("open-popup");
+    stopKeyboard();
+    keyboard.classList.add("disabled");
+}
+
+function closeChallengePopup(){
+    challengePopup.classList.remove("open-popup");
+    startKeyboard();
+    keyboard.classList.remove("disabled");
+}
+
+function getActiveTiles(){                                           //Function for returning the tiles that currently have letters in them
+    return guessGrid.querySelectorAll('[data-state="active"]');
+}
+
+function getAllTiles(){
+    return guessGrid.querySelectorAll(".tile");
+}
+
+function getAllKeys(){
+    return keyboard.querySelectorAll(".key");
+}
+
+function startChallenge(){
+    clearInterval(timer);
+    clearInterval(challengeTimer);
+    playAgain();
+    challengeMode = true;
+    sec = 30;
+    ele.innerHTML = minutes + ':' + sec.toString().padStart(2, '0');
+}
+
 function playAgain(){
+    clearInterval(challengeTimer);
+    clearInterval(timer);
     console.log("Play Again");
     closeWinPopup();
+    closeLosePopup();
     const allTiles = [...getAllTiles()];
     const allKeys = [...getAllKeys()];
     for(let k = 0; k<allTiles.length; k++){
@@ -60,47 +138,48 @@ function playAgain(){
         allKeys[k].classList.remove("wrong-location");
         allKeys[k].classList.remove("wrong");
     }
+
+    getWordle();
     keyboard.classList.remove("disabled");
     startKeyboard();
-    getWordle();
     timerON = false;
+    challengeMode = false;
     minutes = 0;
     sec = 0;
     ele.innerHTML = minutes + ':' + sec.toString().padStart(2, '0');
+
 }
 
 function startTimer(){
     timer = setInterval(()=>{
-        ele.innerHTML = minutes + ':' + sec.toString().padStart(2, '0');
-        if(sec < 59){
-            sec++;
-        }else{
+        sec++;
+        if(sec === 60){
             sec = 0;
             minutes++;
         }
+        ele.innerHTML = minutes + ':' + sec.toString().padStart(2, '0');
     }, 1000);
 }
 
-function openWinPopup(){
-    winPopup.classList.add("open-popup");
+function startChallengeTimer(){
+    challengeTimer = setInterval(()=>{
+        sec--;
+        if(sec === 0){
+            ele.innerHTML = minutes + ':' + sec.toString().padStart(2, '0');
+            clearInterval(challengeTimer);
+            openLosePopup();
+        }
+        ele.innerHTML = minutes + ':' + sec.toString().padStart(2, '0');
+    }, 1000);
 }
-
-function closeWinPopup(){
-    winPopup.classList.remove("open-popup");
-}
-
-function openLosePopup(){
-    losePopup.classList.add("open-popup");
-}
-
-function closeLosePopup(){
-    losePopup.classList.remove("open-popup");
-}
-
 function mouseClick(e){                                         //Handles user mouse clicks
     if(e.target.matches("[data-key]")){
         if(!timerON){
-            startTimer();
+            if(challengeMode){
+                startChallengeTimer();
+            }else{
+                startTimer();
+            }
             timerON = true;
         }
         printKey(e.target.dataset.key);
@@ -121,7 +200,11 @@ function mouseClick(e){                                         //Handles user m
 function keyPress(e){                                          //Handles user keyboard input
     if(e.key.match(/^[a-z]$/) || e.key.match(/^[A-Z]$/)){      //if the key pressed is in between a-z, case insensitive
         if(!timerON){
-            startTimer();
+            if(challengeMode){
+                startChallengeTimer();
+            }else{
+                startTimer();
+            }
             timerON = true;
         }
         printKey(e.key.toLowerCase());
@@ -146,18 +229,6 @@ function printKey(key){                                              //Prints us
     nextTile.dataset.letter = key.toLowerCase();
     nextTile.textContent = key;
     nextTile.dataset.state = "active";
-}
-
-function getActiveTiles(){                                           //Function for returning the tiles that currently have letters in them
-    return guessGrid.querySelectorAll('[data-state="active"]');
-}
-
-function getAllTiles(){
-    return guessGrid.querySelectorAll(".tile");
-}
-
-function getAllKeys(){
-    return keyboard.querySelectorAll(".key");
 }
 
 function deleteKey(){                                               //Function for deleting keys when user hits backspace
@@ -196,24 +267,41 @@ function checkGuess(){                                              //Checks if 
 function submitGuess(guess){                                        //Function that calls the flipTile function for each active tile
     const activeTiles = [...getActiveTiles()];
     stopInteraction();
+    for(let k = 0; k<correctWord.length; k++){
+        let temp = correctWord[k];
+        letterMap.set(temp, letterCount(temp));
+    }
+    for (var [key, value] of letterMap) {
+        console.log(key + " = " + value);
+    }
     activeTiles.forEach((...params) => flipTile(...params, guess));
 }
 
 function flipTile(tile, index, array, guess){
     const letter = tile.dataset.letter.toUpperCase();
     const key = keyboard.querySelector(`[data-key="${letter}"]`);
+    const a = correctWord[index];
     setTimeout(() => {                                              //Start flip animation
         tile.classList.add("flip");
     }, index *  FLIP_ANIMATION_DURATION /2);
 
+    console.log(a + " " + letterMap.get(a));
+    console.log(letterMap.get(a) !== 0);
+
     tile.addEventListener("transitionend", () => {                  //When flip animation ends, remove the flip animation state
         tile.classList.remove("flip");
-        if(correctWord[index] === letter){                          //Check if the letter is in the right spot or not
+        if(a === letter && letterMap.get(a) !== 0){                          //Check if the letter is in the right spot or not
             tile.dataset.state = "correct";
             key.classList.add("correct");
-        }else if(correctWord.includes(letter)){
+            let temp = letterMap.get(letter) - 1;
+            letterMap.set(letter, temp)
+            console.log(letter + " " + letterMap.get(a));
+        }else if(correctWord.includes(letter) && letterMap.get(a) !== 0){
             tile.dataset.state = "wrong-location";
             key.classList.add("wrong-location");
+            let temp = letterMap.get(letter) - 1;
+            letterMap.set(letter, temp)
+            console.log(letter + " " + letterMap.get(a));
         }else{                                                      //If not, then the letter is not in the word
             tile.dataset.state = "wrong";
             key.classList.add("wrong");
@@ -231,19 +319,14 @@ function flipTile(tile, index, array, guess){
 
 function checkWinLose(guess, tiles){
     if(guess.toUpperCase() === correctWord){                                    //If submitted guess matches with the correct word, they win
-        clearInterval(timer);
         openWinPopup();
-        stopKeyboard();
-        keyboard.classList.add("disabled");
         return;
     }
     
     const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])");   //If user has already submitted 6 guesses, then they lose
-    if(remainingTiles === 0){
-        clearInterval(timer);
+    console.log(remainingTiles);
+    if(remainingTiles.length === 0){
         openLosePopup();
-        stopKeyboard();
-        keyboard.classList.add("disabled");
         return;
     }
 }
